ded1faf4d9b0269bc2b41330170c348d81bd74ec
Edwin Lunandy <populationtwo@gmail.com>
2015-05-14 18:06:19 -0400

    Add Navbar to the app

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -5,12 +5,12 @@ app.controller( 'PostsCtrl', function ($scope, $location, Post) {
 
 	$scope.post = {url: 'http://', title: ''};
 
-	$scope.submitPost = function () {
-		Post.create( $scope.post ).then( function (ref) {
-			//$scope.post = {url: 'http://', title: ''};
-			$location.path('/posts/'+ref.name());
-		} );
-	};
+	//$scope.submitPost = function () {
+	//	Post.create( $scope.post ).then( function (ref) {
+	//		//$scope.post = {url: 'http://', title: ''};
+	//		$location.path('/posts/'+ref.name());
+	//	} );
+	//};
 
 	$scope.deletePost = function (post) {
 		Post.delete( post );
